6934963ce31dfb47431ca29c05a888e59e57a2f6
vlad5ss <vlad5grup@gmail.com>
2020-03-16 19:52:56 +0400

    add: Parser implementation
    Parser implementation

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,6 +4,27 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
+    
+    
+    NSString *strText = @"«It’s <an> [example] (string)»";
+    strText = [strText stringByReplacingOccurrencesOfString:@"«" withString:@""];
+    strText = [strText stringByReplacingOccurrencesOfString:@"»" withString:@""];
+    strText = [strText stringByReplacingOccurrencesOfString:@"’" withString:@""];
+    strText = [strText stringByReplacingOccurrencesOfString:@"<" withString:@"«"];
+    strText = [strText stringByReplacingOccurrencesOfString:@">" withString:@"»"];
+    
+    strText = [strText stringByReplacingOccurrencesOfString:@")" withString:@"»"];
+    strText = [strText stringByReplacingOccurrencesOfString:@"(" withString:@"«"];
+    strText = [strText stringByReplacingOccurrencesOfString:@"[" withString:@"«"];
+    strText = [strText stringByReplacingOccurrencesOfString:@"]" withString:@"»"];
+    NSLog(@"%@", strText);
+    
+    
+    //NSString *yourStr= @"Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)";
+    
+    NSArray *arrComponents = [strText componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"« »"]];
+    
+    NSLog(@"arrComponents : %@", arrComponents);
     return @[@"Test"];
 }
 
